9c6492b058186e6feb5047b83269c18812ec9ca0
winardiaris <ariswinardy@gmail.com>
2015-05-15 11:39:48 +0700

    add new theme

--- src/c.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	var 	tema = $("#daftartema"),
-			ttema = tema.html(),
-	      c = ttema.split("|");
-
-			tema.load("tema.txt");
-			$("#daftartemaa").html(c[0]);		
-})
